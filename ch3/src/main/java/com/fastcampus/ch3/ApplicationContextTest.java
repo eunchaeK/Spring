44edf9bdd476ef0bdac4ed3ff85b4d090818df7f
eunchaeK <dhrgml5252@gmail.com>
2022-03-29 20:25:07 +0900

    DI 활용하기 (4)
    @Value 값 주기

--- ch3/src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
+++ ch3/src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
@@ -50,14 +50,44 @@ class Car {
     }
 }
 
+@Component
+@PropertySource("setting.properties")
+class SysInfo{
+    @Value("#{systemProperties['user.timezone']}")
+    String timesZone;
+    @Value("#{systemEnvironment['APPDATA']}")
+    String currDir;
+    @Value("${autosaveDir}")
+    String autosaveDir;
+    @Value("${autosaveInterval}")
+    int autosaveInterval;   //자동저장 간격
+    @Value("${autosave}")
+    boolean autosave;
+
+    @Override
+    public String toString() {
+        return "SysInfo{" +
+                "timesZone='" + timesZone + '\'' +
+                ", currDir='" + currDir + '\'' +
+                ", autosaveDir='" + autosaveDir + '\'' +
+                ", autosaveInterval=" + autosaveInterval +
+                ", autosave=" + autosave +
+                '}';
+    }
+}
+
 public class ApplicationContextTest {
     public static void main(String[] args) {
         ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
 //      Car car = ac.getBean("car", Car.class); // 타입을 지정하면 형변환 안해도됨. 아래의 문장과 동일
         Car car  = (Car) ac.getBean("car"); // 이름으로 빈 검색
-        Car car2 = (Car) ac.getBean(Car.class);   // 타입으로 빈 검색
         System.out.println("car = " + car);
-        System.out.println("car2 = " + car2);
 
+        System.out.println("ac.getBean(SysInfo.class) = " + ac.getBean(SysInfo.class));
+        Map<String, String> map = System.getenv();
+        System.out.println("map = " + map);
+
+        Properties properties = System.getProperties();
+        System.out.println("properties = " + properties);
     }
 }
